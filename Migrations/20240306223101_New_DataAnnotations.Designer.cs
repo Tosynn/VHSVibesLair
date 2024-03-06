1814637d4e0f1573bae59e456b2a64c29c1310ae
Tosin Adewale <tosinadewale01@gmail.com>
2024-03-06 22:45:35 +0000

    New features added!

--- Migrations/20240306223101_New_DataAnnotations.Designer.cs
+++ Migrations/20240306223101_New_DataAnnotations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VHSVibesLair.Data;
 
@@ -11,9 +12,11 @@ using VHSVibesLair.Data;
 namespace VHSVibesLair.Migrations
 {
     [DbContext(typeof(VHSVibesLairContext))]
-    partial class VHSVibesLairContextModelSnapshot : ModelSnapshot
+    [Migration("20240306223101_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,20 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VHSVibesLair.Models
+namespace VHSVibesLair.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; } = string.Empty;
+    public int Id { get; set; }
 
-        [Display (Name = "Release Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+    [StringLength(60, MinimumLength = 1)]
+    public string Title { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
 
-    }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [Range(1, 100), DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+    public string Rating { get; set; } = string.Empty;
 }
